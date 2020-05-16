50a24f4f1a9e1e676b29d1c14fed07e2a78a5578
yamanekei <yamanekei@icroud.com>
2020-05-17 01:11:09 +0900

    修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,9 @@
 class Tweet < ApplicationRecord
   validates :text, presence: true
+  validates :image1, presence: true
+  validates :image2, presence: true
+  validates :image3, presence: true
+  validates :image4, presence: true
   belongs_to :user
   has_many :comments
 
